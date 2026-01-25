4e764748496c53d857951802be737b70e2f879ed
mathfp <matheusfp@protonmail.com>
2026-01-24 23:15:03 -0300

    Microservice de cadastro de insumos

---
+++ src/main/java/br/com/fiap_hackaton_insumos/FiapHackatonInsumosApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap_hackaton_insumos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapHackatonInsumosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapHackatonInsumosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap_hackaton_insumos/FiapHackatonInsumosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap_hackaton_insumos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapHackatonInsumosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
